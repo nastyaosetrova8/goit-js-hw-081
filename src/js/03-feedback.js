fa19ac884a2e089991f85c42261076f8f27d94a6
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2023-06-14 17:01:58 +0200

    add hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 
-const KEY_LS_FORM = "feedback-form-state";
+const KEY_LS_FORM = 'feedback-form-state';
 const inputEmail = form.elements.email;
 const inputMessage = form.elements.message;
 let objForm = JSON.parse(localStorage.getItem(KEY_LS_FORM)) || {};
@@ -10,23 +10,22 @@ let objForm = JSON.parse(localStorage.getItem(KEY_LS_FORM)) || {};
 form.addEventListener('input', throttle(handleForm, 500));
 form.addEventListener('submit', handleSubmit);
 
-
 function handleForm(evt) {
-    objForm[evt.target.name] = evt.target.value;
-    localStorage.setItem(KEY_LS_FORM, JSON.stringify(objForm));
+  objForm[evt.target.name] = evt.target.value;
+  localStorage.setItem(KEY_LS_FORM, JSON.stringify(objForm));
 }
 
 function onLoad() {
-    inputEmail.value = objForm.email || '';
-    inputMessage.value = objForm.message || '';
-    }
-    onLoad();
-
-function handleSubmit(evt){
-    evt.preventDefault()
-    
-    form.reset()
-    localStorage.removeItem(KEY_LS_FORM)
-    console.log(objForm);
+  inputEmail.value = objForm.email || '';
+  inputMessage.value = objForm.message || '';
 }
+onLoad();
 
+function handleSubmit(evt) {
+  evt.preventDefault();
+
+  form.reset();
+  localStorage.removeItem(KEY_LS_FORM);
+  console.log(objForm);
+  objForm = {};
+}
